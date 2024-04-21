ad131f6afaea476adf6a952376b4b97a2e91f216
lyoung <lashellyoung15@gmail.com>
2024-04-21 18:01:48 -0500

    added header component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = (): React.ReactElement => {
+  return (
+    <div>
+      <h1>Alondra Reyna</h1>
+      <a href="/Home.tsx">Home</a>
+    </div>
+  );
+};
+
+export default NavBar;
